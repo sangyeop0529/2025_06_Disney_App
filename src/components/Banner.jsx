66c23cfca1da221452602363f9212826f3a880ec
DELL <sangyeop0529@gmail.com>
2025-06-11 23:59:42 +0900

    배너영역 만들기

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "../api/axios";
+import requests from "../api/requests";
 
 const Banner = () => {
-  return <div>Banner</div>;
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    // 현재 상영 영화 목록 가져오기
+    const response = await axios.get(requests.fetchNowPlaying);
+
+    // 랜덤 영화 선택
+    const movieId =
+      response.data.results[
+        Math.floor(Math.random() * response.data.results.length)
+      ].id;
+
+    // 선택된 영화의 상세 정보 + 비디오 가져오기
+    const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
+      params: { append_to_response: "videos" },
+    });
+
+    console.log(movieDetail);
+
+    setMovie(movieDetail);
+  };
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+        backgroundPosition: "top center",
+        backgroundSize: "cover",
+        height: "500px",
+      }}>
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie.title || movie.name || movie.original_name}
+        </h1>
+
+        <div className="banner__buttons">
+          {movie?.videos?.results[0]?.key && (
+            <button className="banner__button play">Play</button>
+          )}
+        </div>
+        <h1 className="banner__description">{movie.overview}</h1>
+      </div>
+      <div className="banner__fadeBottom"></div>
+    </header>
+  );
 };
 
 export default Banner;
